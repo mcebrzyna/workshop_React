7586ddf455e700bd95bd88aeb04f9922ef703600
Michal Cebrzyna <mcebrzyna@gmail.com>
2018-01-22 12:16:14 +0100

    inicial files

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    entry: ['whatwg-fetch', './js/app.jsx'],
+    output: {
+        filename: "./js/out.js"
+    },
+    devServer:	{
+        inline:	true,
+        contentBase:	'./',
+        port:	3001
+    },
+    watch: true,
+    devtool: 'source-map',
+    module: {
+        loaders: [{
+            test: /\.jsx$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+                presets: ['es2015', 'stage-2', 'react']
+            }
+        }
+        ]
+    }
+}
